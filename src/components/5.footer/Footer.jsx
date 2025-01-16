aca9fe154fa795d96df4d0bebabf4c4d18b2e439
Dania Shawaf <Danya-shwaf>
2025-01-16 11:49:47 +0300

    update

--- src/components/5.footer/Footer.jsx
+++ src/components/5.footer/Footer.jsx
@@ -5,7 +5,6 @@ function Footer() {
         <ul>
             <li>About</li>
             <li>Projects</li>
-            <li>Speaking</li>
             <li>Contact</li>
         </ul>
         <p>&copy; all rights reserved</p>
