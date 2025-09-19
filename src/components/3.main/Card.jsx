dacac0d6ca533fae9c85ae5655abaecd00701941
Dania Shawaf <danyasy990@gmail.com>
2025-09-19 18:56:30 +0300

    update

--- src/components/3.main/Card.jsx
+++ src/components/3.main/Card.jsx
@@ -9,9 +9,10 @@ function Card({filter}) {
             {
                 title: "E_planet Shopping",
                 img: '/images/planet shopping.png',
+                tech: "react redux css",
                 information: "The application is an online plant shopping platform, consisting of three main pages: About Us page Plants Display page Shopping Cart page.",
                 github: "https://github.com/Danya-Shwaf/e-plantShopping.git",
-                linkedIn: "https://www.linkedin.com/posts/danya-shawaf-b36777304_webdevlopment-react-javascript-activity-7270123283335774209-RBoP?utm_source=share&utm_medium=member_desktop"
+                live: "https://e-plant-shopping-eq9g.vercel.app/"
             },
         ]},
         {
@@ -20,48 +21,54 @@ function Card({filter}) {
                 {
                     title: "Social Media Web",
                     img: "/images/social media web.png",
+                    tech: "html css js axios",
                     information: "An open-source social networking platform utilizing APIs to connect users and provide an innovative interactive social experience.",
                     github: "https://github.com/Danya-Shwaf/social_media_web.git",
-                    linkedIn: "https://www.linkedin.com/posts/danya-shawaf-b36777304_webdevelopment-frontenddevelopment-tailwindcss-activity-7251633110453297155-5nvN?utm_source=share&utm_medium=member_desktop"
+                    live: "https://social-media-web-uans.vercel.app/"
                 },
                 {
                     title: "City Prayer Times",
                     img: "/images/prayer time cities.png",
+                    tech: "html css js api",
                     information: "A web applications that displays prayer times based on the selected city using a prayer times API",
                     github: "https://github.com/Danya-Shwaf/city_prayer_times.git",
-                    linkedIn: "https://www.linkedin.com/posts/danya-shawaf-b36777304_webdevelopment-frontenddevelopment-tailwindcss-activity-72516"
+                    live: "https://social-media-web-c5rb.vercel.app/"
                 },
                 {
                     title: "Contact Form",
                     img: "/images/contact form.png",
+                    tech: "html css js",
                     information: "The Contact Form project is an interactive form built with HTML, CSS, and JavaScript to collect user information. It allows users to easily submit their name, email, and message.",
                     github:"https://github.com/Danya-Shwaf/Danya-Shwaf.github.io/tree/main/contact-form",
-                    linkedIn: ""
+                    live: ""
                 }
             ]
         },
         {
             category : "HTML && CSS",
             projects : [{
-            title: "Telegram Page",
-            img: "/images/tele page.png",
-            information: "The Tele-Page project is a static webpage built using HTML and Tailwind CSS. It features a clean and organized design.The layout is fully responsive, ensuring compatibility across various screen sizes.",
-            github: "https://github.com/Danya-Shwaf/Danya-Shwaf.github.io/tree/main/Tele-page",
-            linkedIn: ""
+                title: "Telegram Page",
+                img: "/images/tele page.png",
+                tech: "html css",
+                information: "The Tele-Page project is a static webpage built using HTML and Tailwind CSS. It features a clean and organized design.The layout is fully responsive, ensuring compatibility across various screen sizes.",
+                github: "https://github.com/Danya-Shwaf/Danya-Shwaf.github.io/tree/main/Tele-page",
+                live: "https://telegram-page-dun.vercel.app/"
             },
             {
                 title: "Template One",
                 img: "/images/template one.png",
+                tech: "html tailwaindcss",
                 information: "The Template-One project is a static webpage built with HTML and CSS. It features a clean, responsive design, ideal for personal or business websites.",
                 github: "https://github.com/Danya-Shwaf/Danya-Shwaf.github.io/tree/main/Template-One",
-                linkedIn: ""
+                live: "https://danya-shwaf.github.io/Template-One/"
             },
             {
                 title: "Template Two",
                 img: "/images/template two.png",
+                tech: "html css",
                 information: "The Template-Two project is a static webpage built with HTML and CSS. It offers a simple, elegant, and responsive design, suitable for personal or business websites.",
                 github: "https://github.com/Danya-Shwaf/Danya-Shwaf.github.io/tree/main/Template-Two",
-                linkedIn: ""
+                live: "https://danya-shwaf.github.io/Template-Two/"
             },
             ]
         },
@@ -90,9 +97,8 @@ let displayedProjects = [];
                         <p>{project.information}</p>
                         <hr/>
                         <ul className="links">
-                            {/* <li><a href="" className="icon-link"></a></li> */}
                             <li><a href={project.github} className="icon-github"></a></li>
-                            <li><a href={project.linkedIn} className="icon-linkedin"></a></li>
+                            <li><a href={project.live} className="live">Live Demo</a></li>
                         </ul>
                     </div>
                 </div>
